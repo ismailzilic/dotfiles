1c75c5c63348176129baae9408c4c3fb51a45ee0
Ismail Zilic <ismailzilic@gmail.com>
2025-08-25 14:18:16 +0200

    fixed hypr cfg picker and laptop cfg

--- .config/hypr/select-config.sh
+++ .config/hypr/select-config.sh
@@ -2,9 +2,9 @@
 
 DESKTOP_CONF="$HOME/dotfiles/.config/hypr/hyprland-desktop.conf"
 LAPTOP_CONF="$HOME/dotfiles/.config/hypr/hyprland-laptop.conf"
-HYPR_CONF="$HOME/.config/hypr/hyprland.conf"
+HYPR_CONF="$HOME/dotfiles/.config/hypr/hyprland.conf"
 
-if ls /sys/class/power_supply/ | grep -q "BAT"; then
+if [ -d "/sys/class/power_supply/BAT0" ]; then
     ln -sf "$LAPTOP_CONF" "$HYPR_CONF"
 else
     ln -sf "$DESKTOP_CONF" "$HYPR_CONF"
