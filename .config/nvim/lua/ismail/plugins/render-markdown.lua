9ffada30d2a6697ae039bc6c49acfd7aba327216
Ismail Zilic <ismailzilic@gmail.com>
2025-08-25 15:21:51 +0200

    nvim - added render markdown

---
+++ .config/nvim/lua/ismail/plugins/render-markdown.lua
@@ -0,0 +1,15 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local render_mkdown = require("render-markdown")
+
+		render_mkdown.setup({
+			completions = { blink = { enabled = true } },
+		})
+
+		vim.keymap.set("n", "<leader>mk", function()
+			render_mkdown.toggle()
+		end, { desc = "Toggle render markdown" })
+	end,
+}
