36e9116880b1551568409e594a49cb48842fd582
Ismail Zilić <ismailzilic@gmail.com>
2026-08-04 14:29:14 +0200

    feat: added hypridle & hyprlock

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -4,6 +4,7 @@ local function start_once()
 	hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 	hl.exec_cmd("systemctl --user start graphical-session.target")
 
+	hl.exec_cmd("hypridle")
 	hl.exec_cmd("nm-applet")
 	hl.exec_cmd("waybar")
 	hl.exec_cmd("dunst")
